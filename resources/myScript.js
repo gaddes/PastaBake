ef4c5d5e23f459d2c033396f2d8ef6d62e798f1d
Matthew Gaddes <matthew.gaddes@gmail.com>
2018-04-29 22:34:17 +0100

    Change font size of SideNav and refactor code to enhance readability.

--- resources/myScript.js
+++ resources/myScript.js
@@ -1,19 +1,5 @@
 
 function toggleNav() {
-<<<<<<< HEAD
-    var x = document.getElementById("menu-btn");
-    if (x.className === "btn hide") {
-      /* Open sidenav */
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginRight = "250px";
-      x.className = "btn show"; // Change class name of menu button to reflect the fact that the sidenav is VISIBLE
-    } else {
-      /* Close sidenav */
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginRight = "0";
-      x.className = "btn hide"; // Change class name of menu button to reflect the fact that the sidenav is NOT VISIBLE
-    }
-=======
   var x = document.getElementById("menu-btn");
   if (x.className === "btn hide") {
     /* Open sidenav */
@@ -32,5 +18,4 @@ function toggleNav() {
 function hideNav() {
   var x = document.getElementById("menu-btn");
   x.className = "btn hide";
->>>>>>> WIP
 }
