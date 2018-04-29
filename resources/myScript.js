f65e01007478ac74d1987bc90c5bed55bc6afdb1
Matthew Gaddes <matthew.gaddes@gmail.com>
2018-04-29 22:22:45 +0100

    Change font size of SideNav and refactor code to enhance readability.

--- resources/myScript.js
+++ resources/myScript.js
@@ -1,15 +1,21 @@
 
 function toggleNav() {
-    var x = document.getElementById("menu-btn");
-    if (x.className === "btn hide") {
-      /* Open sidenav */
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginRight = "250px";
-      x.className = "btn show"; // Change class name of menu button to reflect the fact that the sidenav is VISIBLE
-    } else {
-      /* Close sidenav */
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginRight = "0";
-      x.className = "btn hide"; // Change class name of menu button to reflect the fact that the sidenav is NOT VISIBLE
-    }
+  var x = document.getElementById("menu-btn");
+  if (x.className === "btn hide") {
+    /* Open sidenav */
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginRight = "200px";
+    x.className = "btn show"; // Change class name of menu button to reflect the fact that the sidenav is VISIBLE
+  } else {
+    /* Close sidenav */
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    x.className = "btn hide"; // Change class name of menu button to reflect the fact that the sidenav is NOT VISIBLE
+  }
+}
+
+// NOTE: ideally this function will be called when the screen width increases beyond 700px, but  not sure how to acheive this with CSS media queries, therefore the function is currently redundant.
+function hideNav() {
+  var x = document.getElementById("menu-btn");
+  x.className = "btn hide";
 }
