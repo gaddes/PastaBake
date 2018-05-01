1433bd54714ed2f69c0dc4e552091639e0404913
Matthew Gaddes <matthew.gaddes@gmail.com>
2018-05-01 21:48:49 +0100

    Reduce height of header on scroll. Reduce font-size of header text on scroll for narrow width screens only.

--- resources/myScript.js
+++ resources/myScript.js
@@ -19,3 +19,20 @@ function hideNav() {
   var x = document.getElementById("menu-btn");
   x.className = "btn hide";
 }
+
+// Shrink height of header on page scroll
+var header_desktop = document.querySelector('.header-desktop');
+var header_tablet = document.querySelector('.header-tablet');
+var header_mobile = document.querySelector('.header-mobile');
+
+window.addEventListener('scroll', function() {
+  if(window.pageYOffset > 100) {
+    header_desktop.classList += ' shrink'
+    header_tablet.classList += ' shrink'
+    header_mobile.classList += ' shrink'
+  } else {
+    header_desktop.classList = 'header-desktop';
+    header_tablet.classList = 'header-tablet';
+    header_mobile.classList = 'header-mobile';
+  }
+})
